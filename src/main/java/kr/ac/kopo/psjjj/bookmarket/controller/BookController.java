769d00df648d0d384b1f9a55169686d1cfb5d030
seongjun1011 <sj297916@gmail.com>
2026-04-28 10:32:30 +0900

    6장 폼태그를 사용해서 새로운 도서 등록

--- src/main/java/kr/ac/kopo/psjjj/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/psjjj/bookmarket/controller/BookController.java
@@ -49,6 +49,16 @@ public class BookController {
         return "book";
     }
 
+    @PostMapping("/add")
+    public String submitAddNewBook(@ModelAttribute Book book){
+        bookService.setNewBook(book);
+        return "redirect:/books";
+    }
+    @ModelAttribute
+    public void addAddtributes(Model model){
+        model.addAttribute("addTitle", "신규 도서 등록");
+    }
+
     @GetMapping("/all")
     public ModelAndView requestAllBooks() {
         ModelAndView modelAndView = new ModelAndView();
@@ -56,5 +66,10 @@ public class BookController {
         modelAndView.addObject("bookList", list);
         modelAndView.setViewName("books");
         return modelAndView;
+    }
+
+    @GetMapping("/add")
+    public String requestAddBookForm() {
+        return "addBook";
     }
 }
--- src/main/java/kr/ac/kopo/psjjj/bookmarket/repository/BookRepositoryImpl.java
+++ src/main/java/kr/ac/kopo/psjjj/bookmarket/repository/BookRepositoryImpl.java
@@ -113,5 +113,10 @@ public class BookRepositoryImpl implements BookRepository{
 
         return booksByCategory;
     }
+
+    @Override
+    public void setNewBook(Book book) {
+        listOfBooks.add(book);
+    }
 }
 
