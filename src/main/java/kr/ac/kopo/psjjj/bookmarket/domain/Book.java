13a1d5a59a2b00a9f8108502cd1ad551a4112879
seongjun1011 <sj297916@gmail.com>
2026-05-26 11:51:19 +0900

    20260526

--- src/main/java/kr/ac/kopo/psjjj/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/psjjj/bookmarket/domain/Book.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.psjjj.bookmarket.domain;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +13,13 @@ import java.math.BigDecimal;
 @Getter
 
 public class Book {
+    @Pattern(regexp = "ISBN[1-9]+")
     private String bookId; // 도서 id
+    @Size(min = 4, max = 50)
     private String name; // 도서 제목
+    @Min(value = 0)
+    @Digits(integer = 8, fraction = 2)
+    @NotNull
     private BigDecimal unitPrice; // 단가
     private String author; // 저자
     private String description; // 설명
